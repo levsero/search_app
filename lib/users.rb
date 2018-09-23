f9abde64a9fcdff4a904733c3d12b5b40cffa44a
levsero <levsero@gmail.com>
2018-09-23 13:55:00 +1000

    add index by org to users

--- lib/users.rb
+++ lib/users.rb
@@ -2,6 +2,7 @@ class Users
   def initialize(users, organizations, tickets)
     @users = users
     @indexed_by_id = index_by_id
+    @indexed_by_organization = indexed_by_organization
     @organizations = organizations
     @tickets = tickets
   end
@@ -10,6 +11,10 @@ class Users
     @users.select do |user|
       user[field] == value.to_s
     end.map { |user| user.merge(associations_for(user)) }
+  end
+
+  def find_by_organization(organization_id)
+    @indexed_by_organization[organization_id].map { |user_id| find(user_id) }
   end
 
   def find(id)
@@ -28,6 +33,12 @@ class Users
   def index_by_id
     @users.each_with_object({}) do |user, hash|
       hash[user['_id']] = user
+    end
+  end
+
+  def indexed_by_organization
+    @users.each_with_object(Hash.new { |h, k| h[k] = [] }) do |user, hash|
+      hash[user['organization_id']] << user['_id']
     end
   end
 end
--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -55,7 +55,31 @@ describe Users do
       'verified' => true
     }
   end
-  let(:users_hash) { [user_1, user_2] }
+  let(:user_3) do
+    {
+      '_id' => 3,
+      'active' => true,
+      'alias' => 'Miss Joni',
+      'created_at' => '2016-06-23T10:31:39 -10:00',
+      'email' => 'jonibarlow@flotonic.com',
+      'external_id' => 'c9995ea4-ff72-46e0-ab77-dfe0ae1ef6c2',
+      'last_login_at' => '2012-04-12T04:03:28 -10:00',
+      'locale' => 'zh-CN',
+      'name' => 'Cross Barlow',
+      'organization_id' => 2,
+      'phone' => '9575-552-585',
+      'role' => 'end-user',
+      'shared' => false,
+      'signature' => "Don't Worry Be Happy!",
+      'suspended' => false,
+      'tags' => %w[Foxworth Woodlands Herlong Henrietta],
+      'timezone' => 'Melbourne',
+      'url' => 'http://initech.zendesk.com/api/v2/users/2.json',
+      'verified' => true
+    }
+  end
+
+  let(:users_hash) { [user_1, user_2, user_3] }
 
   subject(:users) do
     Users.new(users_hash, organizations, tickets)
@@ -76,7 +100,27 @@ describe Users do
 
     context 'and the user id does not exist' do
       it 'returns the correct user' do
-        expect(subject.find(3)).to equal(nil)
+        expect(subject.find(0)).to equal(nil)
+      end
+    end
+  end
+
+  describe '#find_by_organization' do
+    context 'and the organization has a user' do
+      it 'returns the correct users' do
+        expect(subject.find_by_organization(1)).to eq([user_1])
+      end
+    end
+
+    context 'and the organization has multiple users' do
+      it 'returns the correct users' do
+        expect(subject.find_by_organization(2)).to eq([user_2, user_3])
+      end
+    end
+
+    context 'and the organization has no users' do
+      it 'returns the correct user' do
+        expect(subject.find_by_organization(0)).to eq([])
       end
     end
   end
